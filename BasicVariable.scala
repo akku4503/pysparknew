9166e5b803be1d7df7f03155b3483bad24285f13
Akash <akashst010@gmail.com>
2025-07-31 06:14:40 +0000

    basicvariable

---
+++ BasicVariable.scala
@@ -0,0 +1,8 @@
+object BasicVariables {
+  def main(args: Array[String]): Unit = {
+    // Step 1: Basic Syntax and variables
+    val language: String = "Scala"
+    var version: Double = 2.12
+    println(s"Step 1 : Welcome to $language version $version")
+  }
+}
