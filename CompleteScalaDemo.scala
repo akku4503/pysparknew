df95a369be3ab3916ad6276539aa4fb96077d7f7
Akash <akashst010@gmail.com>
2025-07-31 10:51:36 +0530

    Create  CompleteScalaDemo.scala

---
+++ CompleteScalaDemo.scala
@@ -0,0 +1,9 @@
+%%writefile CompleteScalaDemo.scala
+object CompleteScalaDemo{
+  def main(args: Array[String]) : Unit={
+  // Step1: Basic Syntax and variables
+  val language: String = "Scala"
+  var version: Double = 2.12
+  println(s"Step 1 : Welcome to $language version $version")
+}
+}
